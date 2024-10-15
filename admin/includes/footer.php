4a6189dc3a294fc81aa0a1a65fe2468f2a1bac17
Nerlekaratharva <atharvaabhay10717@gmail.com>
2024-10-15 16:38:43 +0530

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+      <div class="clearfix">
+          <div class="copyright pull-left">Health Care Diagnostic Center @ 2022</div>
+      </div>
+    </footer>
+  </div>
